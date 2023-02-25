6d5310a57e9c06e86fdbf69c4511b65c44fa129b
Robin Garbe <mail@robin-garbe.de>
2023-02-25 15:47:28 +0100

    Removed react router and switched to SCSS

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,21 +1,15 @@
-import { h } from 'preact';
-import { Route, Router } from 'preact-router';
+import { h } from "preact";
 
-import Header from './header';
+import Header from "./header";
 
 // Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import Home from "../routes/home";
 
 const App = () => (
 	<div id="app">
 		<Header />
 		<main>
-			<Router>
-				<Route path="/" component={Home} />
-				<Route path="/profile/" component={Profile} user="me" />
-				<Route path="/profile/:user" component={Profile} />
-			</Router>
+			<Home />
 		</main>
 	</div>
 );
