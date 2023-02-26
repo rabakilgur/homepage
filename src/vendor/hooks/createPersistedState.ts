58aaa4e9a010783ffb3926d722731093ee0b3953
Robin Garbe <mail@robin-garbe.de>
2023-02-26 23:49:38 +0100

    Added monkey-patched hooks

---
+++ src/vendor/hooks/createPersistedState.ts
@@ -0,0 +1,124 @@
+// This is a monkey-patched version of the "use-persisted-state" package by donavon (https://github.com/donavon/use-persisted-state).
+// Patched by Robin Garbe
+
+import { useState, useEffect, useRef, useCallback } from "preact/hooks";
+import useEventListener from "@use-it/event-listener";
+
+/* ----------  createGlobalState:  ---------- */
+
+const globalState = {};
+
+const createGlobalState = (key, thisCallback, initialValue) => {
+	if (!globalState[key]) {
+		globalState[key] = { callbacks: [], value: initialValue };
+	}
+	globalState[key].callbacks.push(thisCallback);
+	return {
+		deregister() {
+			const arr = globalState[key].callbacks;
+			const index = arr.indexOf(thisCallback);
+			if (index > -1) {
+				arr.splice(index, 1);
+			}
+		},
+		emit(value) {
+			if (globalState[key].value !== value) {
+				globalState[key].value = value;
+				globalState[key].callbacks.forEach((callback) => {
+					if (thisCallback !== callback) {
+						callback(value);
+					}
+				});
+			}
+		},
+	};
+};
+
+/* ----------  usePersistedState:  ---------- */
+
+const usePersistedState = (initialState, key, { get, set }) => {
+	const globalState = useRef(null);
+	const [state, setState] = useState(() => get(key, initialState));
+
+	// subscribe to `storage` change events
+	useEventListener("storage", ({ key: k, newValue }: any) => {
+		if (k === key) {
+			const newState = JSON.parse(newValue);
+			if (state !== newState) {
+				setState(newState);
+			}
+		}
+	});
+
+	// only called on mount
+	useEffect(() => {
+		// register a listener that calls `setState` when another instance emits
+		globalState.current = createGlobalState(key, setState, initialState);
+
+		return () => {
+			globalState.current.deregister();
+		};
+	}, [initialState, key]);
+
+	const persistentSetState = useCallback((newState) => {
+		const newStateValue =
+			typeof newState === "function" ? newState(state) : newState;
+
+		// persist to localStorage
+		set(key, newStateValue);
+
+		setState(newStateValue);
+
+		// inform all of the other instances in this tab
+		globalState.current.emit(newState);
+	}, [state, set, key]);
+
+	return [state, persistentSetState];
+};
+
+/* ----------  createStorage:  ---------- */
+
+const createStorage = (provider) => ({
+	get(key, defaultValue) {
+		const json = provider.getItem(key);
+		// eslint-disable-next-line no-nested-ternary
+		return json === null || typeof json === "undefined"
+			? typeof defaultValue === "function"
+			? defaultValue()
+			: defaultValue
+			: JSON.parse(json);
+	},
+	set(key, value) {
+		provider.setItem(key, JSON.stringify(value));
+	},
+});
+
+/* ----------  createPersistentState hook:  ---------- */
+
+const getProvider = () => {
+	if (typeof global !== "undefined" && global.localStorage) {
+		return global.localStorage;
+	}
+	// eslint-disable-next-line no-undef
+	if (typeof globalThis !== "undefined" && globalThis.localStorage) {
+		// eslint-disable-next-line no-undef
+		return globalThis.localStorage;
+	}
+	if (typeof window !== "undefined" && window.localStorage) {
+		return window.localStorage;
+	}
+	if (typeof localStorage !== "undefined") {
+		return localStorage;
+	}
+	return null;
+};
+
+const createPersistedState = (key, provider = getProvider()) => {
+	if (provider) {
+		const storage = createStorage(provider);
+		return (initialState) => usePersistedState(initialState, key, storage);
+	}
+	return useState;
+};
+
+export default createPersistedState;
---
+++ src/vendor/hooks/useDarkMode.ts
@@ -0,0 +1,110 @@
+// This is a monkey-patched version of @fisch0920s patched version (https://github.com/transitive-bullshit/use-dark-mode) of the "use-dark-mode" hook by donavon (https://github.com/donavon/use-dark-mode).
+// Patched by Robin Garbe
+
+import { useCallback, useEffect, useMemo, useState } from "preact/hooks";
+import useEventListener from "@use-it/event-listener";
+import createPersistedState from "./createPersistedState";
+
+/* ----------  Initialize:  ---------- */
+
+const noop = () => {};
+
+const mockElement = {
+	classList: {
+		add: noop,
+		remove: noop,
+	},
+};
+
+const preferDarkQuery = "(prefers-color-scheme: dark)";
+
+const initialize = (storageKey, storageProvider, glbl = global) => {
+	const usePersistedDarkModeState = storageKey
+		? createPersistedState(storageKey, storageProvider)
+		: useState;
+
+	const mql: MediaQueryList = glbl.matchMedia ? glbl.matchMedia(preferDarkQuery) : {} as MediaQueryList;
+
+	const mediaQueryEventTarget = {
+		addEventListener: (_, handler) => mql.addListener && mql.addListener(handler),
+		removeEventListener: (_, handler) => mql.removeListener && mql.removeListener(handler),
+	};
+
+	const isColorSchemeQuerySupported = mql.media === preferDarkQuery;
+
+	const getInitialValue = usersInitialState => (
+		isColorSchemeQuerySupported ? mql.matches : usersInitialState
+	);
+
+	// Mock element if SSR else real body element.
+	const defaultElement = (glbl.document && glbl.document.body) || mockElement;
+
+	const getDefaultOnChange = (
+		element = defaultElement,
+		classNameDark = "dark-mode",
+		classNameLight = "light-mode"
+	) => (val) => {
+		element.classList.add(val ? classNameDark : classNameLight);
+		element.classList.remove(val ? classNameLight : classNameDark);
+	};
+
+	return {
+		usePersistedDarkModeState,
+		getDefaultOnChange,
+		mediaQueryEventTarget,
+		getInitialValue,
+	};
+};
+
+/* ----------  useDarkMode hook:  ---------- */
+
+const useDarkMode = (
+	initialValue = false,
+	{
+		element = undefined,
+		classNameDark = undefined,
+		classNameLight = undefined,
+		onChange = undefined,
+		storageKey = "darkMode",
+		storageProvider = undefined,
+		global = undefined,
+	} = {}
+) => {
+	const {
+		usePersistedDarkModeState,
+		getDefaultOnChange,
+		getInitialValue,
+		mediaQueryEventTarget,
+	} = useMemo(
+		() => initialize(storageKey, storageProvider, global),
+		[storageKey, storageProvider, global]
+	);
+
+	const [state, setState] = usePersistedDarkModeState(getInitialValue(initialValue));
+
+	const stateChangeCallback = useMemo(
+		() => onChange || getDefaultOnChange(element, classNameDark, classNameLight),
+		[onChange, element, classNameDark, classNameLight, getDefaultOnChange]
+	);
+
+	// Call the onChange handler
+	useEffect(() => {
+		stateChangeCallback(state);
+	}, [stateChangeCallback, state]);
+
+	// Listen for media changes and set state.
+	useEventListener(
+		"change",
+		({ matches }: any) => setState(matches),
+		mediaQueryEventTarget as any
+	);
+
+	return {
+		value: state,
+		enable: useCallback(() => setState(true), [setState]),
+		disable: useCallback(() => setState(false), [setState]),
+		toggle: useCallback(() => setState(current => !current), [setState]),
+	};
+};
+
+export default useDarkMode;
