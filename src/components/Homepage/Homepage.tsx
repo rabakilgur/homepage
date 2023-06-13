99c38bc1c84ae5d0f32c8e53494b22810f23c60a
Robin Garbe <mail@robin-garbe.de>
2023-06-13 13:34:27 +0200

    Reworked "Links" and "Kontakt" section

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,4 +1,4 @@
-import { Container, useTheme } from "@nextui-org/react";
+import { Button, Container, useTheme } from "@nextui-org/react";
 import { Fragment, h } from "preact";
 import "../../style/index.scss";
 import DarkModeToggleCornerWrapper from "../DarkModeToggle/DarkModeToggleCornerWrapper";
@@ -116,11 +116,14 @@ const Homepage = () => {
 					<Heading>Links</Heading>
 
 					<div className="blob_box">
-						<a className="blob link_CodePen" href="https://codepen.io/rabakilgur" target="_blank" rel="noreferrer">
-							<span>CodePen</span>
+						<a className="blob link_LinkedIn" href="https://www.linkedin.com/in/robin-garbe/" target="_blank" rel="noreferrer">
+							<span>LinkedIn</span>
 						</a>
 						<a className="blob link_GitHub" href="https://github.com/rabakilgur" target="_blank" rel="noreferrer">
 							<span>GitHub</span>
+						</a>
+						<a className="blob link_CodePen" href="https://codepen.io/rabakilgur" target="_blank" rel="noreferrer">
+							<span>CodePen</span>
 						</a>
 						<a className="blob link_Telegram" href="https://t.me/rabakilgur" target="_blank" rel="noreferrer">
 							<span>Telegram</span>
@@ -134,21 +137,20 @@ const Homepage = () => {
 					<Heading>Kontakt</Heading>
 
 					<div id="contact_section">
-						<div>
-							<i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#0088FF" d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg></i>
-							<span className="contact_heading">Schreib mir:</span>
-							<span className="contact_link"><a href="mailto:kontakt@robin-garbe.de">kontakt@robin-garbe.de</a></span>
-						</div>
-						<div>
-							<i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#0088FF" d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"></path></svg></i>
-							<span className="contact_heading">Ruf mich an:</span>
-							<span className="contact_link"><a href="tel:+4915902450748">+49 159 0245 0748</a></span>
-						</div>
-						<div>
-							<i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#0088FF" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg></i>
-							<span className="contact_heading">Hier bin ich:</span>
-							<span className="contact_link"><a href="https://www.google.com/maps/place/Darmstadt/@49.8748499,8.5847819,12z" target="_blank" rel="noreferrer">Darmstadt</a></span>
-						</div>
+						<span>
+							<div class="contact_box">
+								<i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg></i>
+								<span className="contact_link"><a href="mailto:kontakt@robin-garbe.de">kontakt@robin-garbe.de</a></span>
+							</div>
+							<div class="contact_box">
+								<i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"></path></svg></i>
+								<span className="contact_link"><a href="tel:+4915902450748">+49 159 0245 0748</a></span>
+							</div>
+							<div class="contact_box">
+								<i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg></i>
+								<span className="contact_link"><a href="https://www.google.com/maps/place/Darmstadt/@49.8748499,8.5847819,12z" target="_blank" rel="noreferrer">Darmstadt</a></span>
+							</div>
+						</span>
 					</div>
 				</section>
 
@@ -161,8 +163,6 @@ const Homepage = () => {
 				</footer>
 
 				{/* <between-logo shadow="standard" border-color="#ddd" z-index="20" scale="0.8"></between-logo> */}
-
-				{/* <script src="https://robin-garbe.de/index.js"></script> */}
 			</Container>
 		</>
 	);
