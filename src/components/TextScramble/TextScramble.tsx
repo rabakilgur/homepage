f73d086ed204d06ab5360d555acf96ff056e41e2
Robin Garbe <mail@robin-garbe.de>
2023-06-23 17:24:05 +0200

    Rework TextScramble component
    - Make is slower
    - Don't render on every frame
      - Instead request the next frame after a given time has elapsed
    - Make it more performant
    - Use a monospace font to keep the letters from jumping around so much
    - Avoid using dangerouslySetInnerHTML

--- src/components/TextScramble/TextScramble.tsx
+++ src/components/TextScramble/TextScramble.tsx
@@ -4,73 +4,67 @@ import S from "./TextScramble.module.scss";
 const chars = "!<>-_\\/[]{}—=+*^?#________";
 const randomChar = () => chars[Math.floor(Math.random() * chars.length)];
 
-export default function ScrambleText({ texts, timeout = 1700, ...props }: { texts: string, timeout?: number, [prop: string]: any }) {
+const padArray = (array: any[], size = array.length) => [...array, ...Array(size - array.length).fill(null)];
+
+export default function ScrambleText(
+	{ texts, timeout = 1700, speed = 50, ...props }:
+	{ texts: string, timeout?: number, speed?: number, [prop: string]: any }
+) {
 	const scramblePhrases = texts.split(",").map((text: string) => text.trim());
-	const [text, setText] = useState(scramblePhrases[0]);
-	const frameRequestRef = useRef<number>();
-	let scrambleCounter = 0;
-	let frame: number;
-	let queue: { from: string, to: string, start: number, end: number, char?: string }[];
-	let resolve: (value?: unknown) => void;
+	const longestPhraseLength = Math.max(...scramblePhrases.map((phrase) => phrase.length));
+	const scrambleStates = scramblePhrases.map((phrase) => padArray(phrase.split(""), longestPhraseLength))
+	const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
+	const targetChars = scrambleStates[(currentPhraseIndex + 1) % scrambleStates.length];
+	const [currentChars, setCurrentChars] = useState(scrambleStates[0]);
+	const renderRef = useRef<() => void>(() => {});
+	let intermediateTimeout: number;
+	let animationFrame: number;
 
-	const updateText = (newText: string) => {
-		const oldText: string = text;
-		const length = Math.max(oldText.length, newText.length);
-		const promise = new Promise(res => resolve = res);
-		queue = [];
-		for (let i = 0; i < length; i++) {
-			const from = oldText[i] || "";
-			const to = newText[i] || "";
-			const start = Math.floor(Math.random() * 40);
-			const end = start + Math.floor(Math.random() * 40);
-			queue.push({ from, to, start, end });
-		}
-		cancelAnimationFrame(frameRequestRef.current!);
-		frame = 0;
-		update();
-		return promise;
-	}
+	const render = () => {
+		if (render === renderRef.current) {
+			const possibleIndexes: number[] = [];
+			currentChars.forEach((char, i) => {if (char !== targetChars[i]) possibleIndexes.push(i)});
+			const index = possibleIndexes[Math.floor(Math.random() * possibleIndexes.length)];
+			let newChars: Array<string | object> = [];
 
-	const update = () => {
-		let output = "";
-		let complete = 0;
-		for (let i = 0, n = queue.length; i < n; i++) {
-			let { from, to, start, end, char } = queue[i];
-			if (frame >= end) {
-				complete++;
-				output += to;
-			} else if (frame >= start) {
-				if (!char || Math.random() < 0.28) {
-					char = randomChar();
-					queue[i].char = char;
-				}
-				output += `<span class="${S.dud}">${char}</span>`;
+			if (currentChars[index] !== targetChars[index]) {
+				const newChar = (currentChars[index] === null) || (typeof currentChars[index] === "string")
+					? <span className={S.dud}>{randomChar()}</span>
+					: targetChars[index];
+				newChars = Object.assign([], currentChars, { [index]: newChar });
+				setCurrentChars(newChars);
+			}
+			if (newChars.join("") !== targetChars.join("")) {
+				intermediateTimeout = setTimeout(() => {
+					animationFrame = requestAnimationFrame(renderRef.current)
+				}, speed);
 			} else {
-				output += from;
+				setCurrentPhraseIndex((oldIndex) => (oldIndex + 1) % scrambleStates.length);
 			}
 		}
-		setText(output);
-		if (complete === queue.length) {
-			resolve();
-		} else {
-			frameRequestRef.current = requestAnimationFrame(update);
-			frame++;
-		}
-	}
+	};
 
 	useEffect(() => {
-		let timeoutId: number;
-		const scrambleNext = () => {
-			updateText(scramblePhrases[scrambleCounter]).then(() => {
-				timeoutId = setTimeout(scrambleNext, timeout ?? 1500);
-			});
-			scrambleCounter = (scrambleCounter + 1) % scramblePhrases.length;
-		};
-		scrambleNext();
 		return () => {
-			cancelAnimationFrame(frameRequestRef.current!);
-			clearTimeout(timeoutId);
+			cancelAnimationFrame(animationFrame);
+			clearTimeout(intermediateTimeout);
 		}
 	}, []);
-	return <span dangerouslySetInnerHTML={{ __html: text }} {...props}></span>;
+
+	useEffect(() => {
+		const initialTimout = setTimeout(() => {
+			animationFrame = requestAnimationFrame(renderRef.current);
+		}, timeout);
+		return () => {
+			clearTimeout(initialTimout);
+		}
+	}, [currentPhraseIndex]);
+
+	useEffect(() => {
+		renderRef.current = render;
+	});
+
+	return <span {...props}>{currentChars}</span>;
 }
+
+// TODO: Fix bug where a HMR during a transition would break the entire animation (meaning that multiple updates whould be executed at the same time)
