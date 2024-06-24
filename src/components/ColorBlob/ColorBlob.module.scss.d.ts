8c26940eb0d5e0b474b25654cff6f9056057ba24
Robin Garbe <mail@robin-garbe.de>
2024-06-24 19:45:14 +0200

    Fix bug in Firefox where blob blur was capped to 100px

--- src/components/ColorBlob/ColorBlob.module.scss.d.ts
+++ src/components/ColorBlob/ColorBlob.module.scss.d.ts
@@ -1,5 +1,8 @@
 declare const styles: {
+  readonly "blob": string;
   readonly "colorBlob": string;
+  readonly "inner": string;
+  readonly "wrapper": string;
 };
 export = styles;
 
--- src/components/ColorBlob/ColorBlob.tsx
+++ src/components/ColorBlob/ColorBlob.tsx
@@ -1,23 +1,10 @@
 import S from "./ColorBlob.module.scss";
 
-export default function ColorBlob({
-	color,
-	size,
-	top,
-	right,
-	bottom,
-	left
-}: {
-	color: string,
-	size: number,
-	top?: string,
-	right?: string,
-	bottom?: string,
-	left?: string
-}) {
+export default function ColorBlob({ color, size, top, right, bottom, left }: { color: string; size: number; top?: string; right?: string; bottom?: string; left?: string }) {
+	// It's now pretty, but we need to nest these elements and apply a 100px blur in layers because Firefox caps blur filters at 100px (when softare rendering)
 	return (
 		<div
-			className={S.colorBlob}
+			className={S.wrapper}
 			style={`
 				--color: ${color};
 				--size: ${size};
@@ -25,7 +12,16 @@ export default function ColorBlob({
 				--right: ${right};
 				--bottom: ${bottom};
 				--left: ${left}
-			`}
-		></div>
+			`}>
+			<div className={S.inner}>
+				<div className={S.inner}>
+					<div className={S.inner}>
+						<div className={S.inner}>
+							<div className={S.blob}></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
